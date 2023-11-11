d746db00512d657d4c84432b84af228c4de739f4
lstsk <carsonlin713@gmail.com>
2023-11-11 03:36:46 -0500

    research and course page

--- frontend/app/research/page.tsx
+++ frontend/app/research/page.tsx
@@ -1,7 +1,53 @@
-export default function Research() {
-    return (
-        <div>
-            Hello
+"use client";
+
+import FilterBarResearch from "../component/filterresearch";
+import Search from "../component/search";
+import Table from "../component/table";
+
+export default function Course() {
+
+    const singleFakeData = {
+        title: 'Algorithm and Data Structure',
+        college: 'Swarthmore College',
+        category: 'NS',
+        instructor: 'Vasanta Chaganti',
+        schedule: "Summer 2024"
+      };
+
+    const repeatedFakeData = Array(10).fill(singleFakeData);
+
+
+  return (
+    <div className="flex flex-col">
+      
+
+      {/* Body */}
+      <div className="flex-grow px-28 mt-10">
+        <div className="w-full">
+
+          {/* Filter and search bar */}
+          <div className="flex space-x-4 justify-between">
+            <FilterBarResearch />
+            <Search placeholder="Search for researches" />
+          </div>
+
+          <div className="mt-10">
+
+            {/* Break line and result num */}
+            <div className="relative">
+              <h1 className="text-sm absolute top-1 right-0">Found: 210 Results</h1>
+              <hr className="mt-3 text-gray-200" />
+            </div>
+
+            {/* Actual result */}
+            <div className="mt-10">
+              <Table data={repeatedFakeData}/>
+            </div>
+
+          </div>
         </div>
-    )
+      </div>
+
+    </div>
+  );
 }
