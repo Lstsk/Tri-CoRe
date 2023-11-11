4b3ad3102dc761bc4285cfc2db6c2b0d3418976d
knuceles <son.jaehoon0826@gmail.com>
2023-11-10 19:18:36 -0500

    adding api

---
+++ TricoApi/Controllers/CourseController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+namespace TricoApi.Controllers;
+
+[ApiController]
+[Route("[Controller]")]
+public class CourseController:ControllerBase
+{
+	public CourseController()
+	{
+	}
+
+	[HttpGet]
+	public async Task<IActionResult> GetCourses()
+	{
+		return Ok("courses");
+	}
+}
+
---
+++ TricoApi/Models/Course.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace TricoApi.Models;
+
+public class Course
+{
+	public Guid CourseId { get; set; }
+	public string? CourseName { get; set; }
+}
+
